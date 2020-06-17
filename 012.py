3f7f561fe30da5952b30284c53d98600cd2650be
GustavoJatene <jateneg@gmail.com>
2020-06-17 12:57:16 -0300

    pintando parede

---
+++ 012.py
@@ -0,0 +1,6 @@
+larg = float(input("Digite a largula da parede: "))
+altu = float(input("Digite a altura da parede: "))
+area = larg*altu
+tint = area /2
+print("A dimensão da sua parede é de: {}m²".format(area))
+print("A quantidade de tinta necessária para pintar é de: {}L".format(tint))
