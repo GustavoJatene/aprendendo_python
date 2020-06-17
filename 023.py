5e2c9b7e92d0e6424c05da7fba4043e7d004c83c
GustavoJatene <jateneg@gmail.com>
2020-06-17 16:27:12 -0300

    separando digitos

---
+++ 023.py
@@ -0,0 +1,6 @@
+num = int(input("Digite um numero de até 4 digitos: "))
+un = num //1 % 10
+dez = num //10 % 10
+cen = num //100 % 10
+mil = num // 1000 % 10
+print("Unidade: {}\nDezena: {}\nCentena: {}\nMilhar: {}".format(un, dez, cen, mil))
