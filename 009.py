1a4c881d7faf9891d7d3efd3202faeb3d2907cff
GustavoJatene <jateneg@gmail.com>
2020-06-17 12:50:31 -0300

    corversão de medida

---
+++ 009.py
@@ -0,0 +1,8 @@
+M = float(input("Digite em metros o valor que deseja converter: "))
+Km = M/1000
+Hm = M/100
+Dam = M/10
+dm = M*10
+cm = M*100
+mm = M*1000
+print("{}M em Kilometros é = {}Km\nEm Hectômetros é = {}Hm\nEm Decâmetro é = {}Dam\nEm decímetros é = {}dm\nEm centímetros é = {}cm\nEm milímetros é = {}mm".format(M, Km,Hm,Dam, dm, cm, mm))
