01af2ab18af6c45755d263d22804efd59a5ce972
GustavoJatene <jateneg@gmail.com>
2020-06-17 12:41:24 -0300

    ler dia, mês e ano

---
+++ 003.py
@@ -0,0 +1,7 @@
+#ler o dia, mês e ano de nascimento e mostrar com data formatada.
+
+dia = input("Infome o dia de nascimento: ")
+mes = input("Informe o mês de nascimento: ")
+ano = input("Informe o ano de nascimento: ")
+
+print("Você nasceu no dia {} de {} do ano {}".format(dia, mes, ano))
