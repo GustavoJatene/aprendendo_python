9484d564dfa466c10ebbe33e76e40fc81ef798d4
GustavoJatene <jateneg@gmail.com>
2020-06-17 12:47:40 -0300

    calculo de média

---
+++ 008.py
@@ -0,0 +1,4 @@
+n1 = int(input("Digite a primeira nota: "))
+n2 = int(input("Digite a segunda nota: "))
+#med = (n1+n2)/2 <- (caso precise usar esse valor posteriormente, é preciso criar uma variável)
+print("A média é = {}".format((n1+n2)/2))
