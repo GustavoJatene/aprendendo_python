88ce2c66c6364a1c215aa6f5cd8cb3982176320c
GustavoJatene <jateneg@gmail.com>
2020-06-17 12:46:08 -0300

    dodro, triplo, raiz quadrada

---
+++ 007.py
@@ -0,0 +1,5 @@
+n = int(input("Digite um número: "))
+# n2 = n*2 <- (Caso precise usar esses valores posteriormente, é preciso que crie as variáveis)
+# n3 = n*3
+# raiz = pow(n, 1/2) ou n**1/2
+print('O dobro do numero {} é = {} \nO triplo é = {} \nA raiz quadrada é = {:.2f}'.format(n, (n * 2), (n * 3), (pow(n, 1 / 2))))
