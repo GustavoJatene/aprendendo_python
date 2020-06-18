71d7c399a46dd68c3178caf420e909b2076b1f46
GustavoJatene <jateneg@gmail.com>
2020-06-18 00:11:42 -0300

    Validando o primeiro nome

--- 024.py
+++ 024.py
@@ -1,2 +1,2 @@
 citn = str(input("Informe sua cidade: ")).strip().split()
-print("Validar se nome da cidade começa com santo: {}".format("SANTO" in citn[0].upper()))
+print("Validar se nome da cidade começa com santo: {}".format("santo" in citn[0].lower()))
