8d7e8eb21790bf3cc3bb23c5c1d5c3d516ce5e0d
GustavoJatene <jateneg@gmail.com>
2020-07-16 16:02:16 -0300

    Comparando números

---
+++ 038.py
@@ -0,0 +1,9 @@
+num1 = int(input("Informe o primeiro número: "))
+num2 = int(input("Informe o segundo número: "))
+
+if num1 > num2:
+    print("O PRIMEIRO valor é maior")
+elif num2 > num1:
+    print("O SEGUNDO valor é maior")
+else:
+    print("Valores IGUAIS")
