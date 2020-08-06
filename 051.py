cd3990a0bddbbcdd05674c8a8671f991a0b71c9b
GustavoJatene <jateneg@gmail.com>
2020-08-06 19:36:23 -0300

    Progressão Aritmética

--- 051.py
+++ 051.py
@@ -1,7 +1,21 @@
-pa = []
+print("Calculo de uma Progressão aritmética")
 n = int(input("Infome um numero: "))
+cont = 0
 razao = int(input("Informe a razão: "))
-for c in range (0,10):
-    n += razao
-    pa.append(n)
-print("{}".format(pa))
+for c in range (n,10*razao):
+    print("{}".format(n),end=",")
+    n = n+razao
+    cont+=1
+    if cont == 10:
+        break
+print("FINISH")
+
+'''
+primeiro = int(input("Infome um numero: "))
+razao = int(input("Informe a razão: "))
+decimo = n +(10-1)*razão
+for c in range (primeiro,decimo+razao, razao):
+    print("{}".format(c),end=",")
+print("FINISH")
+SOLUÇÃO DO GUANABARA, A SOLUÇÃO ANTERIOR FOI A QUE ENCONTREI
+'''
