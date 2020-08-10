7b01d2c07b8cd9ac2e2d86d33ad7881e4ffb4cae
GustavoJatene <jateneg@gmail.com>
2020-08-10 18:15:08 -0300

    Detector de Palíndromo

--- 053.py
+++ 053.py
@@ -1,8 +1,23 @@
 frase = str(input("Digite algo: ")).lower().strip().split()
 frase3 = "".join(frase)
 palin = frase3[::-1]
-print(palin)
+print(frase3,palin)
 if frase3 == palin:
     print("Essa frase é um palindromo")
 else:
     print("Não é um palindromo")
+
+'''
+frase = str(input("Digite uma frase: ")).strip().upper()
+palavras = frase,split
+junto = "".join(palavras)
+inverso = ""
+for letra in range(len(junto)-1, -1, -1)
+    inverso +=junto[letra]
+if inverso == junto:
+    print("Temos um palindromo")
+else:
+    print("A frase digitada não é um palindromo")
+    
+    SOLUÇÃO DO CURSO EM VÍDEO
+'''
