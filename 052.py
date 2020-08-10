81ec60305da0d703debe8ccff50f26fc5cd214d1
GustavoJatene <jateneg@gmail.com>
2020-08-10 16:58:23 -0300

    Números primos

--- 052.py
+++ 052.py
@@ -1,7 +1,11 @@
 n = int(input("Digite um numero: "))
-cont = []
+cont = 0
 for c in range(1,n+1):
     if n%c==0:
-        print(c, end=" ")
-        '''cont.append(n%c==0)
-print(cont)'''
+        print("{}".format(c), end=" ")
+        cont += 1
+print("\nO numero é {} foi divisivel {} vezes".format(n, cont))
+if cont == 2:
+    print("Portanto, é primo")
+else:
+    print("Portanto, não é primo")
