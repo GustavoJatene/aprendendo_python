63ef3aabf8267e9319ebd4efd2ac04a15a6e27ed
GustavoJatene <jateneg@gmail.com>
2020-09-16 21:35:26 -0300

    Vários números com flag

--- 066.py
+++ 066.py
@@ -1,8 +1,8 @@
 num = cont = soma = 0
 while True:
     num = int(input("Digite um numero ou 999 para encerrar: "))
-    cont += 1
     if num == 999:
         break
+    cont += 1
     soma += num
 print(f"Foram digitados {cont} numeros e a soma total é de {soma}")
