8764e6f833224ee36bab8fa3a442f33d5ca64305
GustavoJatene <jateneg@gmail.com>
2020-10-04 11:15:02 -0300

    Análise de dados do grupo

---
+++ 069.py
@@ -0,0 +1,24 @@
+cont = 0
+adt = jvm = 0
+contm = 0
+conth = 0
+while True:
+    print('-' * 30)
+    print('CADASTRO DE USUÁRIO')
+    print('-' * 30)
+    age = int(input('INFORME A IDADE: '))
+    gen = str(input('Sexo: [M/F] ')).strip().upper()
+    cont +=1
+    print('-' * 30)
+    if gen == 'M':
+        contm +=1
+    if age >= 18:
+        adt += 1
+    if age <= 20:
+        if gen == 'F':
+            jvm += 1
+    val = str(input('QUER CONTINUAR? [S/N] ')).strip().upper()
+    if val == 'N':
+        break
+print(f'Total de pessoas com mais de 18 anos: {adt}\nQuantidade de homens cadastrados = {contm}\n'
+      f'Quantidade de mulheres com menos de 20 anos = {jvm}')
