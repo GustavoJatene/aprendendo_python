1313d8606fc85f94996650c0924a1909f18b3622
GustavoJatene <jateneg@gmail.com>
2021-01-06 08:31:51 -0300

    Maior e menor valores em Tupla

---
+++ 074.py
@@ -0,0 +1,6 @@
+from random import randint
+numt = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
+
+print(f'Os numeros criados foram: {numt}')
+print(f'Valor máximo é: {max(numt)}')
+print(f'Valor mínimo é: {min(numt)}')
